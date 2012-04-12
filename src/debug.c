329c8f8ccecc2a518097446734f351972884fc43
Zachary Waldowski <zwaldowski@gmail.com>
2012-04-11 23:26:04 -0400

    Silence Clang's "previous prototype" warning.

--- src/debug.c
+++ src/debug.c
@@ -27,6 +27,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void ffi_stop_here(void);
+void ffi_assert(char *expr, char *file, int line);
+void ffi_type_test(ffi_type *a, char *file, int line);
+
 /* General debugging routines */
 
 void ffi_stop_here(void)
